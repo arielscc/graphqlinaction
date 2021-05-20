dd6d1158d7d64a553a58fd349deaf69ce7ed4cf8
ArielScc <lf0ariel@gmail.com>
2021-05-20 02:32:49 -0400

    Create first model Usuario

---
+++ CRMGraphQL/models/Usuario.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const modelUsuario = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
+    trum: true,
+  },
+  apellido: {
+    type: String,
+    required: true,
+    trum: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trum: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trum: true,
+  },
+  creado: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const model = mongoose.model('Usuario', modelUsuario);
+
+module.exports = model;
